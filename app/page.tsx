140b8d17501568c64ffe32f7c97007a8714e67bb
Rowland Adimoha <31654196+rowjay007@users.noreply.github.com>
2023-03-15 22:48:47 +0100

    fix landing page css

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
  
   return (
-          <div>
-      <h1>Welcome to my app!</h1>
-      <h3>Please log in or register to continue</h3>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl md:text-5xl font-bold mb-4">Welcome to my app!</h1>
+      <h3 className="text-lg md:text-3xl mb-8">
+        Please log in or register to continue
+      </h3>
       <UserModal />
     </div>
-  
   );
 }
--- components/Auth/UserModal.tsx
+++ components/Auth/UserModal.tsx
@@ -18,10 +18,15 @@ export default function UserModal() {
           <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
-        <>
-          <LoginModal />
-          <RegisterModal />
-        </>
+          <div className="flex flex-col sm:flex-row">
+  <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mb-2 sm:mr-2">
+    <LoginModal />
+  </button>
+  <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md sm:ml-2">
+    <RegisterModal />
+  </button>
+</div>
+
       )}
     </>
   );
