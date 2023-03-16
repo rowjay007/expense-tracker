d3721b6d5a0251c0e954ec5481b309b355d16bea
Rowland Adimoha <31654196+rowjay007@users.noreply.github.com>
2023-03-16 10:48:48 +0100

    feat: add styles to login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 "use client"
-import React, { useState } from "react";
+import React from "react";
 import { Inter } from "next/font/google";
 import UserModal from "@/components/Auth/UserModal";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
- 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl md:text-5xl font-bold mb-4">Welcome to my app!</h1>
-      <h3 className="text-lg md:text-3xl mb-8">
-        Please log in or register to continue
-      </h3>
-      <UserModal />
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-3xl md:text-5xl font-bold mb-4">
+          Welcome to my app!
+        </h1>
+        <h3 className="text-lg md:text-3xl mb-8">
+          Please log in or register to continue
+        </h3>
+        <UserModal />
+      </div>
     </div>
   );
 }
--- components/Auth/LoginModal.tsx
+++ components/Auth/LoginModal.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 import { useForm } from "react-hook-form";
 import { auth } from "@/utils/firebase";
 import { userState } from "@/utils/state";
+import { MdClose } from "react-icons/md";
 
 type LoginFormValues = {
   email: string;
@@ -35,29 +36,43 @@ export default function LoginModal() {
     <>
       <button onClick={() => setIsOpen(true)}>Login</button>
       {isOpen && (
-        <div>
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit(handleLogin)}>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                {...register("email", { required: true })}
-              />
-              {errors.email && <span>Email is required</span>}
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg p-8">
+            <div className="flex justify-end">
+              <button onClick={() => setIsOpen(false)}>
+                <MdClose />
+              </button>
             </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                {...register("password", { required: true })}
-              />
-              {errors.password && <span>Password is required</span>}
-            </div>
-            <button type="submit">Login</button>
-          </form>
+            <h2 className="text-2xl font-bold mb-4">Login</h2>
+            <form onSubmit={handleSubmit(handleLogin)}>
+              <div className="mb-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  {...register("email", { required: true })}
+                  className="border border-gray-300 rounded-lg p-2 w-full"
+                />
+                {errors.email && <span>Email is required</span>}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  {...register("password", { required: true })}
+                  className="border border-gray-300 rounded-lg p-2 w-full"
+                />
+                {errors.password && <span>Password is required</span>}
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md"
+              >
+                Login
+              </button>
+            </form>
+          </div>
         </div>
       )}
     </>
