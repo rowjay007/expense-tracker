05fd17fea77d893e3d3a9c7fcbe339afac414ce9
Rowland Adimoha <31654196+rowjay007@users.noreply.github.com>
2023-03-22 21:56:59 +0100

    fix: restart

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-"use client"
-import React from "react";
-import { Inter } from "next/font/google";
-import UserModal from "@/components/Auth/UserModal";
+import React from 'react'
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="text-3xl md:text-5xl font-bold mb-4">
-          Welcome to my app!
-        </h1>
-        <h3 className="text-lg md:text-3xl mb-8">
-          Please log in or register to continue
-        </h3>
-        <UserModal />
-      </div>
-    </div>
-  );
+    <div>Home</div>
+  )
 }
+
+export default Home
